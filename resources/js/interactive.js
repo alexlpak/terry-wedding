80e20bbdd2c8da00808a113323feed449ed01b4d
Alex Pak <alexlpak@gmail.com>
2019-07-14 02:09:00 -0500

    further action items layout and mobile adjustment

--- resources/js/interactive.js
+++ resources/js/interactive.js
@@ -12,6 +12,8 @@ $('#guestName').keypress(function(e){
   }
 });
 
+
+
 function tabNewGuest() {
   $(".userInput").on('keydown', '#guestName', function(e) {
     var keyCode = e.keyCode || e.which;
@@ -54,3 +56,18 @@ $(document).ready(function(){
     });
   });
 });
+
+// Detect if element in viewport
+function isElementInViewport(el) {
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return console.log(
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
